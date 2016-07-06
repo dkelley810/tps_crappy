5050de631c0811a39f0875096de8cf28efd56701
Robbie Ferguson <contact@category5.tv>
2016-07-06 10:38:35 -0400

    Randomly pick skin

--- mods/crappy_skins/init.lua
+++ mods/crappy_skins/init.lua
@@ -1,8 +1,10 @@
 minetest.register_on_joinplayer(function(player)
-	local filename = minetest.get_modpath("player_textures").."/textures/player_"..player:get_player_name()
-	local f = io.open(filename..".png")
+	local numberOfSkins = 1 ' Set the number of skins in ./textures
+	local number = math.random(numberOfSkins)
+	local filename = minetest.get_modpath("crappy_skins").."/textures/"..number..".png"
+	local f = io.open(filename)
 	if f then
 		f:close()
-		default.player_set_textures(player, {"player_"..player:get_player_name()..".png"})
+		default.player_set_textures(player, {number..".png"})
 	end
 end)
