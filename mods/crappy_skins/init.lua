04b44b2ee06c6ccc8379b60b19a2830bee7252d8
Robbie Ferguson <contact@category5.tv>
2016-07-06 10:43:25 -0400

    Change variable name

--- mods/crappy_skins/init.lua
+++ mods/crappy_skins/init.lua
@@ -1,10 +1,10 @@
 minetest.register_on_joinplayer(function(player)
 	local numberOfSkins = 1 -- Set the number of skins in ./textures
-	local number = math.random(numberOfSkins)
-	local filename = minetest.get_modpath("crappy_skins").."/textures/"..number..".png"
+	local chosenSkin = math.random(numberOfSkins)
+	local filename = minetest.get_modpath("crappy_skins").."/textures/"..chosenSkin..".png"
 	local f = io.open(filename)
 	if f then
 		f:close()
-		default.player_set_textures(player, {number..".png"})
+		default.player_set_textures(player, {chosenSkin..".png"})
 	end
 end)
