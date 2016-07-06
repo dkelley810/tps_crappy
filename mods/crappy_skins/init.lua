b8530f712b7db33da8357963dffc51ff99f5d471
Robbie Ferguson <contact@category5.tv>
2016-07-06 10:42:07 -0400

    Wrong language
    Sometimes I forget I'm working in Lua  :)

--- mods/crappy_skins/init.lua
+++ mods/crappy_skins/init.lua
@@ -1,5 +1,5 @@
 minetest.register_on_joinplayer(function(player)
-	local numberOfSkins = 1 ' Set the number of skins in ./textures
+	local numberOfSkins = 1 -- Set the number of skins in ./textures
 	local number = math.random(numberOfSkins)
 	local filename = minetest.get_modpath("crappy_skins").."/textures/"..number..".png"
 	local f = io.open(filename)
